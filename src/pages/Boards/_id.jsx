95d069294650ef3bb75559d59292e684ed182edb
EdQuinx <bjnboo833626@gmail.com>
2024-04-28 02:35:58 +0700

    Refactor structure #2

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,5 +1,4 @@
 import Container from '@mui/material/Container'
-import Box from '@mui/material/Box'
 import AppBar from '../../components/AppBar'
 import BoardBar from './BoardBar'
 import BoardContent from './BoardContent'
